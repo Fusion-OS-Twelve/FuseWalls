88ba23f9dca4bc39ea51204e93c32048a87d6f20
naufalw <naufalwiwitputra@gmail.com>
2022-01-07 23:43:48 +0700

    init model

---
+++ lib/app/data/models/wall_model.dart
@@ -0,0 +1,8 @@
+import 'package:fuse_walls/app/data/enums/wall_enums.dart';
+
+class WallModel {
+  String path;
+  WallType walltype;
+
+  WallModel({required this.path, required this.walltype});
+}
