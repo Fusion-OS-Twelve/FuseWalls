4b1a3972be2ef8c107036d5d25c6c46c6e2c8987
naufalw <naufalwiwitputra@gmail.com>
2022-01-08 21:37:02 +0700

    initial material you support

--- lib/app/modules/preview_wall/preview_wall.dart
+++ lib/app/modules/preview_wall/preview_wall.dart
@@ -1,5 +1,6 @@
 import 'package:adaptive_action_sheet/adaptive_action_sheet.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:fuse_walls/app/data/providers/theme_provider.dart';
 import 'package:fuse_walls/app/data/services/theme_services.dart';
 import 'package:get/get.dart';
@@ -19,134 +20,138 @@ class PreviewWall extends GetView<PreviewWallController> {
         Get.back;
         return true;
       },
-      child: Scaffold(
-        body: SafeArea(
-          child: Column(
-            children: [
-              Container(
-                height: 80,
-                alignment: Alignment.center,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    GestureDetector(
-                      child: const Icon(Icons.arrow_back_ios_new),
-                      onTap: () {
-                        Get.back();
-                      },
-                    ),
-                    const Text(
-                      "Preview",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                          fontFamily: "Satisfy",
-                          fontSize: 32,
-                          shadows: [
-                            Shadow(
-                              offset: Offset(0, 4.0),
-                              blurRadius: 4.0,
-                              color: Color(0x25000000),
-                            ),
-                          ]),
-                    ),
-                    GestureDetector(
-                        onTap: () => showAdaptiveActionSheet(
-                            context: context, actions: themeSelectorAction),
-                        child: Icon(themeMode.value == ThemeMode.light
-                            ? Icons.wb_sunny
-                            : Icons.mode_night)),
-                  ],
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                height: height - 80 - 80,
-                child: PinchZoomImage(
-                  image: Image.asset(
-                    controller.path,
-                    isAntiAlias: true,
-                    filterQuality: FilterQuality.high,
-                  ),
-                ),
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 10),
-                height: 80,
-                child: Center(
+      child: AnnotatedRegion(
+        value: SystemUiOverlayStyle(
+            systemNavigationBarColor: Get.theme.scaffoldBackgroundColor),
+        child: Scaffold(
+          body: SafeArea(
+            child: Column(
+              children: [
+                Container(
+                  height: 80,
+                  alignment: Alignment.center,
                   child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      SizedBox(
-                        height: 50,
-                        child: TextButton(
-                            style: TextButton.styleFrom(
-                                shape: const CircleBorder()),
-                            onPressed: () {
-                              controller.shareWallpaper();
-                            },
-                            child: const Icon(
-                              Icons.share,
-                              size: 22,
-                            )),
+                      GestureDetector(
+                        child: const Icon(Icons.arrow_back_ios_new),
+                        onTap: () {
+                          Get.back();
+                        },
                       ),
-                      SizedBox(
-                        height: 50,
-                        child: TextButton(
-                          onPressed: () {
-                            showAdaptiveActionSheet(
-                                context: context,
-                                androidBorderRadius: 20,
-                                actions: [
-                                  BottomSheetAction(
-                                      title: const Text("Home Screen"),
-                                      onPressed: () {
-                                        controller.setWallpaper(location: 1);
-                                      }),
-                                  BottomSheetAction(
-                                      title: const Text("Lock Screen"),
-                                      onPressed: () {
-                                        controller.setWallpaper(location: 2);
-                                      }),
-                                  BottomSheetAction(
-                                      title: const Text("Home & Lock Screen"),
-                                      onPressed: () {
-                                        controller.setWallpaper(location: 3);
-                                      })
-                                ]);
-                          },
-                          child: const Text(
-                            "SET WALLPAPER",
-                            style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.w900,
-                                color: Colors.white),
-                          ),
-                          style: TextButton.styleFrom(
-                              padding: const EdgeInsets.all(14),
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20)),
-                              backgroundColor: const Color(0xFF42B0FF)),
-                        ),
+                      const Text(
+                        "Preview",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontFamily: "Satisfy",
+                            fontSize: 32,
+                            shadows: [
+                              Shadow(
+                                offset: Offset(0, 4.0),
+                                blurRadius: 4.0,
+                                color: Color(0x25000000),
+                              ),
+                            ]),
                       ),
-                      SizedBox(
-                        height: 50,
-                        child: TextButton(
-                            onPressed: () {
-                              controller.copyToDownloadsFoler();
-                            },
-                            style: TextButton.styleFrom(
-                                shape: const CircleBorder()),
-                            child: const Center(
-                                child: Icon(
-                              Icons.download,
-                              size: 22,
-                            ))),
-                      )
+                      GestureDetector(
+                          onTap: () => showAdaptiveActionSheet(
+                              context: context, actions: themeSelectorAction),
+                          child: Icon(themeMode.value == ThemeMode.light
+                              ? Icons.wb_sunny
+                              : Icons.mode_night)),
                     ],
                   ),
                 ),
-              )
-            ],
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  height: height - 80 - 80,
+                  child: PinchZoomImage(
+                    image: Image.asset(
+                      controller.path,
+                      isAntiAlias: true,
+                      filterQuality: FilterQuality.high,
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  height: 80,
+                  child: Center(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(
+                          height: 50,
+                          child: TextButton(
+                              style: TextButton.styleFrom(
+                                  shape: const CircleBorder()),
+                              onPressed: () {
+                                controller.shareWallpaper();
+                              },
+                              child: const Icon(
+                                Icons.share,
+                                size: 22,
+                              )),
+                        ),
+                        SizedBox(
+                          height: 50,
+                          child: TextButton(
+                            onPressed: () {
+                              showAdaptiveActionSheet(
+                                  context: context,
+                                  androidBorderRadius: 20,
+                                  actions: [
+                                    BottomSheetAction(
+                                        title: const Text("Home Screen"),
+                                        onPressed: () {
+                                          controller.setWallpaper(location: 1);
+                                        }),
+                                    BottomSheetAction(
+                                        title: const Text("Lock Screen"),
+                                        onPressed: () {
+                                          controller.setWallpaper(location: 2);
+                                        }),
+                                    BottomSheetAction(
+                                        title: const Text("Home & Lock Screen"),
+                                        onPressed: () {
+                                          controller.setWallpaper(location: 3);
+                                        })
+                                  ]);
+                            },
+                            child: const Text(
+                              "SET WALLPAPER",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w900,
+                                  color: Colors.white),
+                            ),
+                            style: TextButton.styleFrom(
+                                padding: const EdgeInsets.all(14),
+                                shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20)),
+                                backgroundColor: Get.theme.backgroundColor),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 50,
+                          child: TextButton(
+                              onPressed: () {
+                                controller.copyToDownloadsFoler();
+                              },
+                              style: TextButton.styleFrom(
+                                  shape: const CircleBorder()),
+                              child: const Center(
+                                  child: Icon(
+                                Icons.download,
+                                size: 22,
+                              ))),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:dynamic_color/dynamic_color.dart';
+import 'package:dynamic_colorscheme/dynamic_colorscheme.dart';
 import 'package:flutter/material.dart';
 import 'package:fuse_walls/app/data/providers/theme_provider.dart';
 import 'package:fuse_walls/app/routes/pages.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:material_color_utilities/material_color_utilities.dart';
 
 import 'app/routes/routes.dart';
 
@@ -16,13 +19,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      getPages: AppPages.pages,
-      title: 'FuseWalls',
-      initialRoute: Routes.MENU,
-      theme: ThemeData.light().copyWith(),
-      darkTheme: ThemeData.dark().copyWith(),
-      themeMode: getThemeMode(),
-    );
+    ColorScheme? m3Light;
+    ColorScheme? m3Dark;
+    return DynamicColorBuilder(builder: (CorePalette? palette) {
+      if (palette != null) {
+        m3Light = DynamicColorScheme.generate(palette, dark: false);
+        m3Dark = DynamicColorScheme.generate(palette, dark: true);
+      }
+      return GetMaterialApp(
+        getPages: AppPages.pages,
+        title: 'FuseWalls',
+        initialRoute: Routes.MENU,
+        theme: m3Light == null
+            ? ThemeData(colorScheme: ThemeData.light().copyWith().colorScheme)
+            : ThemeData(colorScheme: m3Light),
+        darkTheme: m3Dark == null
+            ? ThemeData(colorScheme: ThemeData.dark().copyWith().colorScheme)
+            : ThemeData(colorScheme: m3Dark),
+        themeMode: getThemeMode(),
+      );
+    });
   }
 }
